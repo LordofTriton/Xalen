0702c2843a419aa3f82a8ef3267c809058891fa8
LordofTriton <agboola918@gmail.com>
2022-06-17 01:26:13 -1100

    Fixed 'intelligence' typo :)

--- src/components/infoPage/infoPage.js
+++ src/components/infoPage/infoPage.js
@@ -24,7 +24,7 @@ const InfoPage = ({CortexControl}) => {
                         <h3 className="infoTitle">Who is Xalen?</h3>
                         <h3 className="infoText">Xalen is your friend.</h3>
                         <h3 className="infoText">A snarky, sarcastic, sometimes idiotic friend, but a friend nonetheless. 😅</h3>
-                        <h3 className="infoText">Xalen is a self learning conversational chatbot designed to hold funny and witty conversations with users through artificial intellignece methods that enable the program to communicate like an actual human being.</h3>
+                        <h3 className="infoText">Xalen is a self learning conversational chatbot designed to hold funny and witty conversations with users through artificial intelligence methods that enable the program to communicate like an actual human being.</h3>
                         <h3 className="infoText">Xalen learns from each and every conversation, becoming better and more intelligent, improving it's ability to hold conversations like a boss. 😎</h3>
                         <br/>
                         <br/>

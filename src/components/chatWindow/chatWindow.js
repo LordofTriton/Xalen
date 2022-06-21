72a3daf6ffc0ce4a0c67630cfd7598243bccd6f1
LordofTriton <agboola918@gmail.com>
2022-06-21 04:59:30 -1100

    Fixed APUUrl issues.

--- src/components/chatWindow/chatWindow.js
+++ src/components/chatWindow/chatWindow.js
@@ -18,7 +18,7 @@ let d = new Date();
 let premierSpeaker = Math.random() * 10 > 3;
 // premierSpeaker = true;
 let baseAPIURL = "https://xalen-server.herokuapp.com/";
-baseAPIURL = "http://localhost:5000/";
+// baseAPIURL = "http://localhost:5000/";
 
 const ChatWindow = ({CortexControl}) => {
     const [chatHistory, setChatHistory] = useState([])
